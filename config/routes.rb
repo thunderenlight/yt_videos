aa6a4b90e9165266f440b86cf5fb561d21aa503d
thunderenlight <thunderenlight@gmail.com>
2016-09-09 13:56:42 -0500

    Adding user and omniauth2

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
 
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
 	resources :videos, only: [:index, :new, :create]
 	root to: 'videos#index'
+
+	get '/auth/:provider/callback', to: 'sessions#create'
+	delete '/logout', to: 'sessions#destroy', as: :logout
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
